199e1080ae6b7423f1480e57849e463dff2c1bc4
Wes Peter <wes@wespeter.com>
2019-06-08 10:23:25 -0700

    add left drawer

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ const playInnerButton = document.getElementById("play-inner-button");
  */
 const mainElement = document.getElementById("main");
 
+/**
+ * @type Element
+ */
+const menuButton = document.getElementById("menu-button");
+
+/**
+ * @type Element
+ */
+const drawer = document.getElementById("drawer");
+
 audio.addEventListener("timeupdate", () => {
   // if 1 second left in audio track reset track to 0
   // so it's continuous
@@ -43,4 +53,15 @@ audio.addEventListener("play", () => {
 audio.addEventListener("pause", () => {
   playInnerButton.classList.remove("pause");
   mainElement.classList.remove("playing");
+});
+
+// side drawer setup
+menuButton.addEventListener("opened", ev => {
+  drawer.open = true;
+});
+drawer.addEventListener("open", ev => {
+  menuButton.open = true;
+});
+drawer.addEventListener("close", ev => {
+  menuButton.open = false;
 });
